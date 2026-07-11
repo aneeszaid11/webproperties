ecc3c4b968a0d94fc2a3520a703cf304e044e9cd
aneeszaid11 <anees.rehman@te.com>
2026-07-11 16:17:15 +0530

    Modify browser launch and page navigation in test.py
    Updated browser launch options to run in non-headless mode and simplified page navigation.

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
-    browser = p.chromium.launch(channel="msedge", headless=True)
-
-    page = browser.new_page()
-
-    response = page.goto(
-        "http://tycoident.com",
-        wait_until="networkidle"
+    browser = p.chromium.launch(
+        channel="msedge",
+        headless=False
     )
 
-    print("Status:", response.status)
-    print("Final URL:", page.url)
+    page = browser.new_page()
+    page.goto("http://tycoident.com")
+
+    print(page.url)
 
     browser.close()
