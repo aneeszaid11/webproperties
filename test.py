bbb99e8a44b59d3667c8ab2125a5ee9f69cd512e
aneeszaid11 <anees.rehman@te.com>
2026-07-11 16:19:07 +0530

    Launch browser in headless mode
    Change browser launch to headless mode.

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
     browser = p.chromium.launch(
-        channel="msedge",
-        headless=False
+        headless=True
     )
 
     page = browser.new_page()
+
     page.goto("http://tycoident.com")
 
     print(page.url)
